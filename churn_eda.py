1f711d51a0bc2334686d13293c7c2265070269e9
Troy Zhang <troyz0213@gmail.com>
2024-07-31 16:51:22 -0400

    Data Cleaning Complete

--- churn_eda.py
+++ churn_eda.py
@@ -1,3 +1,16 @@
 # Exploratory Data Analysis (EDA) for the customer churn dataset
+import pandas as pd 
+import numpy as np 
+import seaborn as sns 
+import matplotlib.ticker as mtick
+import matplotlib.pyplot as plt 
+
+# load data
+telco_data = pd.read_csv("/Users/tonyzhang/Desktop/customerchurn/data/WA_Fn-UseC_-Telco-Customer-Churn.csv")
+print(telco_data.dtypes) 
+# there are a lot of object data types, so we need to perform feature engineering to convert them to numerical data types. 
+
+# descriptive statistics on numerical variables
+telco_data.describe()
 
 
